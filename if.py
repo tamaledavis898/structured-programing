49212c5375a52fa721adcd66786485a75a21866f
tamaledavis898 <tamaledavis898@gmail.com>
2024-10-14 06:48:35 +0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,80 @@
+#correct_password=8989
+#attempt=int(input("Enter passwrod:"))
+#if attempt==correct_password:
+#    print("access granted")
+#else:
+#    print("access denied")
+
+# number=int(input("Enter number:"))
+# if number%2 ==0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")    
+
+# n=5
+# while n>0:
+#     print(n)
+#     n=n-1
+# print('blastoff')
+# print(n)
+
+# for i in range(7):
+#     if(i==3):
+#      break
+#     print(f"{i}")
+# else:
+#    print("loop completed")
+
+# for i in range(1,7):
+#     for j in range(1,7):
+#         print(f"{i}*{j}= {i*j}")
+
+# number=float(input("Enter the number:"))
+# if number%2==0:
+#     print("even")
+# else:
+#     print("odd")4
+
+# A=float(input("Enter the number:"))
+# B=float(input("Enter the number:"))
+# diff=B-A
+# if B>A:
+#     print(f"B is greater than A by {diff}")
+# else:
+#     ()
+
+# x=float(input("Enter the number:"))
+# square=x**0.5
+# if square==12:
+#     print(f"perfect square and square root is{square}")
+# else:
+#     print("id not a perfect square root")
+
+# A=float(input("Enter the number:"))
+# B=float(input("Enter the number:"))
+# C=float(input("Enter the number:"))
+# if A!=B:
+#     print(f"{A}")
+#     if B!=C:
+#         print(f"{B}")
+#         if C!=A:
+#          print(f"{C}")
+
+# side1=float(input("Enter the number:"))
+# side2=float(input("Enter the number:"))
+# side3=float(input("Enter the number:"))
+# if side1==side2 and side2==side3:
+#     print('equilateral')
+# else:
+#     print("isaslice")
+
+x=float(input("entre number "))
+a=1
+b=2
+c=3
+w=4
+no_digits=a+b+c+w 
+if no_digits==10:
+    print("correct sum")
+else:
+    print ("wrong sum ")
